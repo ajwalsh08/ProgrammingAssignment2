5ae27b10a0fc41efa48c2a7f4007761fb1cd0656
alex walsh <alexander.walsh@gmail.com>
2014-04-23 19:07:48 -0500

    Created a pair of functions that invert a matrix in a memory-efficient way.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A pair of fcns that inverts a matrix in a memory-efficient way.
+## First, assign makeCacheMatrix(your_matrix) to a variable.
+## Then apply cacheSolve to that new variable.
 
-## Write a short comment describing this function
-
+## This function creates a list of functions to better utilize R's cache.
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set = set, get = get,
+        setsolve = setsolve,
+        getsolve = getsolve)
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    matrix <- x$get()
+    m <- solve(matrix, ...)
+    x$setsolve(m)
+    m
 }
